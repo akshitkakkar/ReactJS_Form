856595f47e44cab834e33ce2f7865698f5590cd5
Akshit Kakkar <Akshit88191@Mastek.Com>
2021-01-17 16:28:10 +0530

    Included redux,axios and sass packages and implemented a basic skeleton of the form in question

---
+++ src/components/Form.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+const Form = () => {
+    return(
+        <div className="form-login">
+            <form>
+                <h3>Sign In</h3>
+                <div className="form-field">
+                    <label>First Name</label>
+                    <input type="text" name="first-name"/>
+                </div>
+                <div className="form-field">
+                    <label>Last Name</label>
+                    <input type="text" name="last-name"/>
+                </div>
+                <div className="form-checkbox">
+                    <input type="checkbox" tabindex="0"/>
+                    <label>Remember Me?</label>
+                </div>
+                <button class="form-submit" type="submit">Sign In</button>
+                <div className="form-help">
+                    <a href="#">Forgot your password?</a>
+                    <p>Don't have an account? <a href="#">Sign up</a></p>
+                    <a href="#">Resend Email Confirmation</a>
+                </div>
+            </form>
+        </div>
+    )
+}
+
+export default Form
